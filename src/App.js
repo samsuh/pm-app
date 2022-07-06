d311993be0b8b98753bec3f01b81b28d22f1e252
samuel suh <samsuh@gmail.com>
2022-07-06 13:25:51 +0900

    add react-router-dom but version 5.1 for now

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import './App.css'
+
+import Dashboard from './pages/dashboard/Dashboard'
+import Login from './pages/login/Login'
+import Signup from './pages/signup/Signup'
+import Create from './pages/create/Create'
+import Project from './pages/project/Project'
 
 function App() {
   return (
-    <div className="App">
-
+    <div className='App'>
+      <BrowserRouter>
+        <div className='container'>
+          <Switch>
+            <Route exact path='/'>
+              <Dashboard />
+            </Route>
+            <Route path='/create'>
+              <Create />
+            </Route>
+            <Route path='/login'>
+              <Login />
+            </Route>
+            <Route path='/signup'>
+              <Signup />
+            </Route>
+            <Route path='/projects/:id'>
+              <Project />
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
 export default App
