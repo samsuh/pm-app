2832c9639e95d2e14edc3cbe40170f88c7cbbde5
samuel suh <samsuh@gmail.com>
2022-07-06 11:59:20 +0900

    setup and initialize firebase, firestore, auth, timestamp

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDtzTXRCq85-btERzV7gCcHpIZDwyMc3_Q',
+  authDomain: 'web3-pm.firebaseapp.com',
+  projectId: 'web3-pm',
+  storageBucket: 'web3-pm.appspot.com',
+  messagingSenderId: '232361649984',
+  appId: '1:232361649984:web:a6c97b629ffc354bfffca5',
+}
+
+//initialize firebase
+firebase.initializeApp(firebaseConfig)
+
+//init services: firestore and auth
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+
+//timestamp
+const timestamp = firebase.firestore.Timestamp
+
+export { projectFirestore, projectAuth, timestamp }
