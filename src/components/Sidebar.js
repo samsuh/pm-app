2ada8752849a7e2126b7ef0e12bce2eefdfa762a
samuel suh <samsuh@gmail.com>
2022-07-09 13:38:52 +0900

    display user in sidebar from useAuthContext

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,16 +1,19 @@
 import './Sidebar.css'
 import DashboardIcon from '../assets/dashboard_icon.svg'
 import AddIcon from '../assets/add_icon.svg'
+import Avatar from './Avatar'
 
 import { NavLink } from 'react-router-dom'
+import { useAuthContext } from '../hooks/useAuthContext'
 
 export default function Sidebar() {
+  const { user } = useAuthContext()
   return (
     <div className='sidebar'>
       <div className='sidebar-content'>
         <div className='user'>
-          {/* avatar and username goes here */}
-          <p>Hi user</p>
+          <Avatar src={user.photoURL} />
+          <p>Hi {user.displayName}</p>
         </div>
         <nav className='links'>
           <ul>
